a015e63685133fd04866866b3634c86ce64a69bf
BarathwajAnandan <banandan@alumni.cmu.edu>
2024-11-12 23:19:04 -0800

    Removed unnecessary files

--- StreamSage/youtube-audio-recorder/complete.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener((message) => {
-    if (message.type === "audioReady") {
-      const audio = document.getElementById('recordedAudio');
-      audio.src = message.audioUrl;
-    }
-  });
-  
